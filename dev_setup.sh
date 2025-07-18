067c98efcac25259f9e03bf265a86b4b7b2b128b
Michael Foster <50351736+mjfos2r@users.noreply.github.com>
2025-07-17 20:59:34 -0400

    fixed bad tar flags, bumped version, and copied dev_setup.sh over

---
+++ dev_setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# howdy. this script is gonna initialize the dev environment so that everything works properly.
+
+
+# next we should set up the env and install our linters and whatnot.
+uv venv
+source .venv/bin/activate
+uv pip install -r dev_deps.txt
+
+# now install the pre-commit hooks
+pre-commit install
+
+# echo the command to activate the venv and exit cleanly :)
+
+echo "Dev environment has been setup! execute the following command to begin:"
+echo "source .venv/bin/activate"
+echo "If you forgot to rename this directory, do so, delete .git && .venv then try running this script again!"
+exit 0
